378f5d32bd7bcefdc378660d627a70b9d952ba00
Zakharov Dmitry <goddeuce1@gmail.com>
2019-09-28 21:58:05 +0300

    [EDIT] Header struct instead of unordered map

---
+++ src/common/header.h
@@ -0,0 +1,17 @@
+//
+// Created by gd1 on 27.09.2019.
+//
+
+#pragma once
+
+#include <string>
+
+#ifndef HTTP_SERVER_HEADER_H
+#define HTTP_SERVER_HEADER_H
+
+#endif //HTTP_SERVER_HEADER_H
+
+struct header {
+    std::string key;
+    std::string value;
+};
